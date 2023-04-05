c12ce76d9cdbad899e9e793fbfbebd3d93e40328
filoscop <129909124+filoscop@users.noreply.github.com>
2023-04-05 04:07:50 +0200

    Add files via upload

---
+++ src/commands/Community/ping.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+ 
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('pong or smt')
+    .setDMPermission(false),
+    async execute(interaction, client) {
+        const embed = new EmbedBuilder()
+        .setDescription(`:ping_pong: | Pong! Latency: **${client.ws.ping} MS**`)
+        .setColor("Blue")
+        .setTimestamp();
+        
+        await interaction.reply({ embeds: [embed] })
+    }
+}
