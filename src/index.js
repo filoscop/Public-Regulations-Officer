227f6468d7df88e4b9cdbbb0ebfe4d4d6aece507
Henry <121696146+RedKZX@users.noreply.github.com>
2023-05-30 01:41:06 +0100

    add: added self written anti crash with data storing

--- src/index.js
+++ src/index.js
@@ -27,3 +27,96 @@ const figlet = require("figlet")
 figlet.text("REDKZX", function (err, data){
  console.log(data)
 });
+
+//Anti Crash
+
+client.on("error", (err) => {
+    const ChannelID = "1112903325298458654";
+    const Embed = new EmbedBuilder()
+      .setColor("Red")
+      .setTimestamp()
+      .setFooter({ text: "Anti Crash system" })
+      .setTitle("Error Encountered");
+    const Channel = client.channels.cache.get(ChannelID);
+    if (!Channel) return;
+    Channel.send({
+      embeds: [
+        Embed.setDescription(
+          "**Discord API Error/Catch:\n\n** ```" + err + "```"
+        ),
+      ],
+    });
+  });
+  
+  process.on("unhandledRejection", (reason, p) => {
+    const ChannelID = "1112903325298458654";
+    console.log("Unhandled promise rejection:", reason, p);
+    const Embed = new EmbedBuilder()
+      .setColor("Red")
+      .setTimestamp()
+      .setFooter({ text: "Anti Crash system" })
+      .setTitle("Error Encountered");
+    const Channel = client.channels.cache.get(ChannelID);
+    if (!Channel) return;
+    Channel.send({
+      embeds: [
+        Embed.setDescription(
+          "**Unhandled Rejection/Catch:\n\n** ```" + reason + "```"
+        ),
+      ],
+    });
+  });
+  
+  process.on("uncaughtException", (err, origin) => {
+    const ChannelID = "1112903325298458654";
+    const Embed = new EmbedBuilder()
+      .setColor("Red")
+      .setTimestamp()
+      .setFooter({ text: "Anti Crash system" })
+      .setTitle("Error Encountered");
+    const Channel = client.channels.cache.get(ChannelID);
+    if (!Channel) return;
+    Channel.send({
+      embeds: [
+        Embed.setDescription(
+          "**Uncought Exception/Catch:\n\n** ```" + err + "```"
+        ),
+      ],
+    });
+  });
+  
+  process.on("uncaughtExceptionMonitor", (err, origin) => {
+    const ChannelID = "1112903325298458654";
+    const Embed = new EmbedBuilder()
+      .setColor("Red")
+      .setTimestamp()
+      .setFooter({ text: "Anti Crash system" })
+      .setTitle("Error Encountered");
+    const Channel = client.channels.cache.get(ChannelID);
+    if (!Channel) return;
+    Channel.send({
+      embeds: [
+        Embed.setDescription(
+          "**Uncaught Exception Monitor/Catch:\n\n** ```" + err + "```"
+        ),
+      ],
+    });
+  });
+  
+  process.on("warning", (warn) => {
+    const ChannelID = "1112903325298458654";
+    const Embed = new EmbedBuilder()
+      .setColor("Red")
+      .setTimestamp()
+      .setFooter({ text: "Anti Crash system" })
+      .setTitle("Error Encountered");
+    const Channel = client.channels.cache.get(ChannelID);
+    if (!Channel) return;
+    Channel.send({
+      embeds: [
+        Embed.setDescription(
+          "**Warning/Catch:\n\n** ```" + warn + "```"
+        ),
+      ],
+    });
+  });
