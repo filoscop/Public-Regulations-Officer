a6b41da41304e2f41f9bdbd886ffe5f159a181df
Henry <121696146+RedKZX@users.noreply.github.com>
2023-05-31 01:14:10 +0100

    add: added index

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+var colors = require('@colors/colors')
+
+const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
+const {User, Message, GuildMember, ThreadMember, Channel } = Partials;
+
+const {loadEvents} = require('./Handlers/eventHandler');
+const {loadCommands} = require('./Handlers/commandHandler');
+const { LoadErrorHandler } = require("./Handlers/errorHandler");
+
+const client = new Client({
+    intents: [Guilds, GuildMembers, GuildMessages],
+    partials: [User, Message, GuildMember, ThreadMember],
+});
+
+client.commands = new Collection();
+client.errors = new Collection()
+client.config = require('./config.json');
+
+client.login(client.config.token).then(() => {
+    loadEvents(client);
+    loadCommands(client);
+});
